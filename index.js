90aaf0a80e07641b043846113d061541bf5a345c
Khoi <kdng96@gmail.com>
2022-07-05 15:58:49 -0700

    alternatives

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //INTERVIEW TIPS AND TRICKS
 //// 2 Sum
-//BETTER WAY: HASH TABLES GIVES YOU MORE SPACE, makes it run faster and more efficiently
-//MOST OPTIMAL: USING SORT IN THE BEGINNING and using WHILE LOOP
+//BETTER WAY: HASH TABLES GIVES YOU MORE SPACE, makes it run faster and more efficiently (value time more than space)
+//MOST OPTIMAL: USING SORT IN THE BEGINNING and using WHILE LOOP (value space more than time)
