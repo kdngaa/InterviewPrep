36c864e938f82e3022637c6d30d3d65dc214bc8d
Khoi <kdng96@gmail.com>
2022-07-08 23:09:46 -0700

    update

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 //BETTER WAY: HASH TABLES GIVES YOU MORE SPACE, makes it run faster and more efficiently (value time more than space)
 //MOST OPTIMAL: USING SORT IN THE BEGINNING and using WHILE LOOP (value space more than time)
 //REVIEW BIG-O, KNOWING HOW TO READ OPTIMAL SOLUTIONS IS A BIG PLUS
+//
