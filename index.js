373565d3869600b02eb9a89188a0b18a57d1aebd
Khoi <kdng96@gmail.com>
2022-08-10 16:22:58 -0700

    CSRF attack

--- index.js
+++ index.js
@@ -29,8 +29,8 @@
 //Same-Origin Policy: Restricts scripts from interacting with resources from other origins (exp: http and https have different origins)
 //Network requests can be made with same origin policy but responses won't be readable
 //EXP: a banking website and a banking server (client and server) havea relationship, then the banking website is also somehow connected to a bad website
-//Cont. exp: the bad website can also make requests to the banking server (same origin policy can block the bad website to read the response it gives back since it's from a different origin)
-
+//Cont. exp: the bad website can also make GET requests to the banking server (same origin policy can block the bad website to read the response it gives back since it's from a different origin)
+//Cont. exp: however, when the bad website make a POST requests (transfer wire to steal information), this is a CSRF attack (Cross Forge Origin Attack)
 
 
 
