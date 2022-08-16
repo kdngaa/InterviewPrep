86d9d1605c32341a10f6fc20b9c3b5888abcab06
Khoi <kdng96@gmail.com>
2022-08-15 20:21:40 -0700

    IP address

--- index.js
+++ index.js
@@ -65,7 +65,7 @@
 //SYSTEM DESIGN
 //There are no wrong or right answers, but it's your job to defend your stance on why you would do something over something else
 //System basics: Client Server model, Network Protocols (how they communicate with one another)
-//When your Browser first try to type an address for a site (ie Algo Expert), it makes a DNS query behind the scene to figure out an IP address of the site to learn how to communicate with the server
+//When your Browser first try to type an address for a site (ie Algo Expert), it makes a DNS query behind the scene to figure out an IP address (unique identifier for a specific machine) of the site to learn how to communicate with the server
 //
 
 
