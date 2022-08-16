8104de83f1bb75baa2bca70a137e17f1f076adaf
Khoi <kdng96@gmail.com>
2022-08-15 20:03:49 -0700

    System Design

--- index.js
+++ index.js
@@ -60,6 +60,14 @@
 
 
 
+
+
+//SYSTEM DESIGN
+
+
+
+
+
 //// 2 Sum EASY
 //BETTER WAY: HASH TABLES GIVES YOU MORE SPACE, makes it run faster and more efficiently (value time more than space)
 //MOST OPTIMAL: USING SORT IN THE BEGINNING and using WHILE LOOP (value space more than time)
