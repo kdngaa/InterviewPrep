5ab243d842f788aa8c4f7b654a8ac11dba05b4b0
Khoi <kdng96@gmail.com>
2022-10-14 15:38:44 -0700

    Searching left to right

--- index.js
+++ index.js
@@ -211,3 +211,4 @@
 //breathd first traversal: going across before going deeper, still left to right
 //using queue for breath first: like a line of people, things enter in the back of the queue and exits in the front of the queue, no one can skip line
 //queue and stacks are used to travel through and check the tree, you don't often return it directly
+//searching from left to right is very important
