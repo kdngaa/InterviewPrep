9f0bbb8ccfb4f2dda745fec199632723472eae11
Khoi <kdng96@gmail.com>
2022-10-18 15:31:44 -0700

    json objects

--- index.js
+++ index.js
@@ -219,3 +219,4 @@
 
 //FRONTEND EXPERT
 //Array Prototypes review (maps, filter, reduce, filter)
+//json objects have name and value pairs of any type (string, number, boolean, array, objects(unordered)), name however are often strings
