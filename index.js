e700ed3550f74df026ff4c8e6ef99b7b4405405e
Khoi <kdng96@gmail.com>
2022-11-09 15:12:30 -0800

    page

--- index.js
+++ index.js
@@ -251,4 +251,4 @@
 //make sure the API is in JSON, as ARRAYS of OBJECTS
 //create state for all the values in the API (useState)
 //hasNext property is used to see if the API page has a next page
-//whenever the page changes, the page has to make a request to the API, with the use of useEffect
+//whenever the page changes, the page has to make a request to the API, with the use of useEffect, passing in "page" as the dependency array, where the effect will take place when the page changes
