d8da470c3cd14f91508b7de942422f41effc3f66
Khoi <kdng96@gmail.com>
2022-11-09 14:45:07 -0800

    useState

--- index.js
+++ index.js
@@ -249,3 +249,4 @@
 //CRYPTO PRICES REACT MINI PROJECT WITH API
 //declare a variable as a global scope with the API link you'd like to implement
 //make sure the API is in JSON, as ARRAYS of OBJECTS
+//create state for all the values in the API (useState)
